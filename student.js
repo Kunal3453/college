10b3b5d79d956dbb94ba5b2b501e4ac959abf01e
KunalAgrawal <111216500+Kunal3453@users.noreply.github.com>
2022-10-14 15:38:50 +0530

    Add files via upload

---
+++ student.js
@@ -0,0 +1,16 @@
+Array=[{
+    name:"Kunal agrawal",
+    branch:"B.Tech",
+rollno:11
+},{
+    name:"Pushkar",
+    branch:"B.Tech",
+    rollno:12
+},{
+    name:"sakiv",
+    branch:"B.tech",
+    rollno:15
+}]
+console.log(Array[0]);
+console.log(Array[1]);
+console.log(Array[2]);
